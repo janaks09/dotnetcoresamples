8b1bc276f4b130f293fc81f9a522a680ebe4dcfd
Janak Shrestha <old_ink@hotmail.com>
2016-07-15 16:37:51 +0545

    added swagger sample

---
+++ src/SwaggerSample/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerSample.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        /// <summary>
+        /// API to get all values
+        /// </summary>
+        /// <remarks>
+        /// This API will get all values
+        /// </remarks>
+        /// <returns>All values</returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// API that takes Enum
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="subscriptionStatus"></param>
+        /// <returns></returns>
+        [HttpGet("{subscriptionStatus}")]
+        public string Get(SubscriptionStatus subscriptionStatus)
+        {
+            return $"{subscriptionStatus} as enum";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public enum SubscriptionStatus
+    {
+        Free = 0,
+        Paid = 1
+    }
+}
---
+++ src/SwaggerSample/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.Swagger.Model;
+
+namespace SwaggerSample
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            Environment = env;
+        }
+
+        public IConfigurationRoot Configuration { get; }
+        public IHostingEnvironment Environment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string swaggerCommentXmlPath = string.Empty;
+            if (Environment.IsDevelopment()) //while development
+                swaggerCommentXmlPath = $@"{Environment.ContentRootPath}\bin\Debug\netcoreapp1.0\SwaggerSample.xml";
+            else //while production
+                swaggerCommentXmlPath = $@"{Environment.ContentRootPath}\SwaggerSample.xml";
+
+
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddSwaggerGen();
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.SingleApiVersion(new Info
+                {
+                    Version = "v1",
+                    Title = "My awesome API",
+                    Description = "My Awesome API by @janaks09",
+                    TermsOfService = "NA",
+                    Contact = new Contact() { Name="Your name", Email="your email", Url="your url" }
+                });
+
+                options.IncludeXmlComments(swaggerCommentXmlPath); //Includes XML comment file
+                options.DescribeAllEnumsAsStrings();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUi();
+        }
+    }
+}
